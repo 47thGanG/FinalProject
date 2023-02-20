65a08a48d0e7c52beb61ff5a3067b040efea6153
Sergey Bektimirov <85190121+47thGanG@users.noreply.github.com>
2023-02-20 15:00:58 +0600

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#minikube start
+#Get IP agent (use in Prometheus and Grafana)
 IP=$(wget -qO- eth0.me)
 touch 1
 #Build Docker
@@ -55,24 +55,3 @@ sudo systemctl enable grafana
 kubectl apply -f secret.yaml
 kubectl apply -f db-deployment.yaml
 kubectl apply -f web-deployment.yaml
-
-###
-#kubectl expose service my-prom-prometheus-server --type=NodePort --target-port=9090 --name=prometheus-server-np
-#kubectl expose service grafana --type=NodePort --target-port=3000 --name=grafana-np
-
-###по яндексу
-#helm install trickster tricksterproxy/trickster --namespace default -f trickster.yaml
-#kubectl apply -f grafana.yaml
-
-#MKIP=$(minikube ip)
-#CURL=$(curl $MKIP:30000)
-#echo -e "\ncurl $MKIP:30000\n $CURL"
-#MYIP=$(hostname -I)
-#MYIPgoto=($(echo $MYIP | tr " " "\n"))
-#echo -e "\ngo to ==> http://${MYIPgoto[0]}:8080/api/v1/namespaces/default/services/http:web:/proxy/\n\nor use this IP:"
-#for i in "${MYIPgoto[@]}"
-#do
-#    echo -e "$i"
-#done
-#echo -e ""
-#kubectl proxy --address='0.0.0.0' --port=8080 --accept-hosts='.*'
